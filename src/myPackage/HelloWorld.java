41c76963c52746cea30832fa21397af69bbfd950
farida ghezali <pinoukadouche@gmail.com>
2019-05-04 17:41:11 -0400

    this is a second commit

--- src/myPackage/HelloWorld.java
+++ src/myPackage/HelloWorld.java
@@ -7,6 +7,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// this is a comment 
       System.out.println("this is my first commit to a branch");
-    
+    // this is  another comment
 }
 }
